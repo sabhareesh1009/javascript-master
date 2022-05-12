551fbc578ecb544d3ea0dc59755490eec915337f
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2022-05-13 00:16:54 +0530

    Designed event bubling and delgations

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // hosting
 
-let a = 10;
-function calc() {
-  let a = 3;
-  a++;
-  console.log(a);
-  function min() {
-    let a = 6;
-    console.log(a);
-  }
-  min();
-}
-calc();
+// let a = 10;
+// function calc() {
+//   let a = 3;
+//   a++;
+//   console.log(a);
+//   function min() {
+//     let a = 6;
+//     console.log(a);
+//   }
+//   min();
+// }
+// calc();
+
+document.querySelector('#grand-parent').addEventListener('click', ()=> {
+  console.log("grand parent clicked")
+}, true);
+
+document.querySelector('#parent').addEventListener('click', ()=> {
+  console.log(" parent clicked")
+}, true);
+
+document.querySelector('#child').addEventListener('click', ()=> {
+  console.log(" child clicked")
+}, true);
 
 
 
---
+++ prototypes.js
@@ -0,0 +1,3 @@
+// Every object in JavaScript has a built-in property, which is called its prototype. 
+// The prototype is itself an object, so the prototype will have its own prototype, 
+// making what's called a prototype chain.
